71331cf6ad5b25b3764c6ab495e2727b1c2fc459
Anna Dybas <a.m.dybas@gmail.com>
2020-03-13 23:34:31 -0400

    added Searchbar componenet

---
+++ src/components/Searchbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+import { changeSearchText } from "../redux/actions";
+
+const Searchbar = props => (
+  <div className="">
+    <div className="">
+      <input
+        type="text"
+        placeholder="Search"
+        value={props.value}
+        onChange={e => props.onChange(e.target.value)}
+      />
+    </div>
+    <div className="" />
+  </div>
+);
+
+const mapStateToProps = state => {
+  return {
+    value: state.searchText
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onChange: (searchText) => {dispatch(changeSearchText(searchText))}
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Searchbar);
