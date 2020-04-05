e907a7f524544cc183a1d45ad488f02e235f953f
Anna Dybas <a.m.dybas@gmail.com>
2020-04-04 23:59:46 -0400

    added dispatch action to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import RandomRecipe from '../redux/actions'
+import fetchingRandomRecipe from '../redux/actions'
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 // Home page => should show Two boxes, one to generate random recipe (get insprired!),
 // second box should show options to login, with text == find recipes and favorite them to save to your profile
 
 const Home = props => {
     return (
         <div className="">
-            
+            <div>
+                {/* check to see if loged in? if not, show login text and random stuff */}
+            </div>
+
+            <div>
+                {/* Redirect to recipes/:id 
+                    Show button or something that will render a random user recipe!
+                */}
+            </div>
         </div>
     )
 } 
 
- export default Home 
+const mapDispatchToProps = dispatch => {
+    return {
+        randomRecipe: () => {dispatch(fetchingRandomRecipe())}
+    }
+}
+
+
+export default connect(null, mapDispatchToAction)(Home)
+
