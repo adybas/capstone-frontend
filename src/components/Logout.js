68f12b3018d6a9cacc12f3a0e3b69bdc1f3e7603
Anna Dybas <a.m.dybas@gmail.com>
2020-04-14 16:40:21 -0400

    added notes and props to logout component

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,20 +2,22 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {loggingOut} from '../redux/actions'
 
-const Logout = () => {
+//instead of rendering a new page, maybe just render a logout button?! in navBar
+const Logout = (props) => {
+  console.log("logout", props)
     return(
-        <div>
+        <div className="logout">
             I'm a logout page!
         </div>
     )
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-      logout: () => {
-        dispatch(loggingOut())
-      }
-    };
+  return {
+    logout: () => {
+      dispatch(loggingOut())
+    }
   };
+};
 
 export default connect(null, mapDispatchToProps)(Logout)
